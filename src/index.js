437e786fcf2f235f52641a6410cadf073f48a716
Gregori Mikina <gregorimikina@gmail.com>
2021-07-06 00:35:00 +0200

    Create few function and start base stopWatch

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/index.css';
 import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>,
-  document.getElementById('root'));
+ReactDOM.render(<App/>,  document.getElementById('root'));
 
-  // serviceWorker.unregister();
+
 
